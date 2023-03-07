ee011b59e5c082e1674c88ac350de8b6315e60f6
unknown <m.gontarek@wrseti.pl>
2023-03-07 23:50:04 +0100

    Change debug

--- kidflow/settings.py
+++ kidflow/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xnp3i*2l%v7cl-bfzo8xipz(3ni0(8fb3beext41=cjj@_nf+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
